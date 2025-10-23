038a45f4362ead3cd95437242b3675e2a837db05
iguerchal <louis.leborgne2@gmail.com>
2025-10-23 05:51:11 +0200

    fix base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
+    base: '/Moodboard_Studio/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
